89e53123fc0266a7916ac3a5c77b29fe7aee5e5a
Deversatorg <leguenkoav@gmail.com>
2026-01-12 23:34:34 +0100

    NotFound_page

--- app.py
+++ app.py
@@ -4,30 +4,17 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 def init_db():
-<<<<<<< HEAD
-    """Creates the table if it doesn't exist."""
-=======
-    """Initializes the database with a settings table."""
->>>>>>> 02ce89a8799b1736e9c9ffdfc758eb995c10513a
     with sqlite3.connect('wear_app.db') as conn:
         cursor = conn.cursor()
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS user_settings (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 weight REAL,
-<<<<<<< HEAD
                 height REAL,
                 timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
             )
         ''')
         conn.commit()
-=======
-                height REAL
-            )
-        ''')
-        conn.commit()
-
->>>>>>> 02ce89a8799b1736e9c9ffdfc758eb995c10513a
 init_db()
 
 @app.route("/")
@@ -35,7 +22,6 @@ def home():
     current_weight = None
     current_height = None
 
-<<<<<<< HEAD
     with sqlite3.connect('wear_app.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT weight, height FROM user_settings ORDER BY id DESC LIMIT 1")
@@ -47,8 +33,6 @@ def home():
 
     return render_template("index.html", weight=current_weight, height=current_height)
 
-=======
->>>>>>> 02ce89a8799b1736e9c9ffdfc758eb995c10513a
 @app.route("/settings", methods=["GET", "POST"])
 def settings():
     if request.method == "POST":
@@ -61,11 +45,7 @@ def settings():
                            (weight_input, height_input))
             conn.commit()
             
-<<<<<<< HEAD
         return redirect(url_for('stats'))
-=======
-        return redirect(url_for('home'))
->>>>>>> 02ce89a8799b1736e9c9ffdfc758eb995c10513a
 
     return render_template("settings.html")
 
@@ -80,6 +60,10 @@ def stats():
         history_data = cursor.fetchall()
 
     return render_template("stats.html", history=history_data)
+
+@app.route("/try-on")
+def try_on():
+    return render_template("not_found.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
